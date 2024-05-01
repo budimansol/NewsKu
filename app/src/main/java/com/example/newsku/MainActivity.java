9b79ffbbf978f5a1f80c53104d9aee7856a07795
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-05-02 00:50:27 +0700

    Show The News

--- app/src/main/java/com/example/newsku/MainActivity.java
+++ app/src/main/java/com/example/newsku/MainActivity.java
@@ -1,26 +1,58 @@
 package com.example.newsku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Article> articleList = new ArrayList<>();
+    NewsRecyclerAdapter adapter;
+    LinearProgressIndicator progressIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.news_recycler_view);
+        progressIndicator = findViewById(R.id.progress_bar);
+
+
+        setUpRecyclerView();
         getNews();
     }
 
+    void setUpRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new NewsRecyclerAdapter(articleList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    void changeInProgress(boolean show){
+        if (show)
+            progressIndicator.setVisibility(View.VISIBLE);
+        else
+            progressIndicator.setVisibility(View.INVISIBLE);
+    }
+
     void getNews(){
+        changeInProgress(true);
         NewsApiClient newsApiClient = new NewsApiClient("a8840dc95d4b4eb781c936a6221fba2a");
         newsApiClient.getTopHeadlines(
                 new TopHeadlinesRequest.Builder()
@@ -29,8 +61,11 @@ public class MainActivity extends AppCompatActivity {
                 new NewsApiClient.ArticlesResponseCallback() {
                     @Override
                     public void onSuccess(ArticleResponse response) {
-                        response.getArticles().forEach((a)-> {
-                            Log.i("Article", a.getTitle());
+                        runOnUiThread(()->{
+                            changeInProgress(false);
+                            articleList = response.getArticles();
+                            adapter.updateData(articleList);
+                            adapter.notifyDataSetChanged();
                         });
                     }
 
