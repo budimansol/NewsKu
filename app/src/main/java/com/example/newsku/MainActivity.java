96bfc787e3da846b234d751d0e8a84d714edd95e
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-05-01 21:28:20 +0700

    News API

--- app/src/main/java/com/example/newsku/MainActivity.java
+++ app/src/main/java/com/example/newsku/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.newsku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getNews();
     }
+
+    void getNews(){
+        NewsApiClient newsApiClient = new NewsApiClient("a8840dc95d4b4eb781c936a6221fba2a");
+        newsApiClient.getTopHeadlines(
+                new TopHeadlinesRequest.Builder()
+                        .language("id")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        response.getArticles().forEach((a)-> {
+                            Log.i("Article", a.getTitle());
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.i("GAGAL RESPOND", Objects.requireNonNull(throwable.getMessage()));
+                    }
+                }
+        );
+    }
+
 }
